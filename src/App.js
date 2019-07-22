0ed33198d658bc048e62a4dacaf81ac1f28bbccb
Dave Irwin <david@redirwin.com>
2019-07-22 12:08:19 -0700

    Wrapped with context and passed in values. Refactored.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import ProductContext from "./contexts/ProductContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+  return (
+    <div className="App">
+      <ProductContext value={{ products, addItem }}>
+        <Navigation cart={cart} />
+        {/* Routes */}
+        <Route exact path="/" component={Products} />} />
+        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      </ProductContext>
+    </div>
+  );
 }
 
 export default App;
